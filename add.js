df8558b1251bf0be8bdafebe80b0ea27a5c5d9e8
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-10-11 18:49:54 +0200

    initial commit

---
+++ add.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 2));
+// testSum(1, "2");
+// sum('asdf', 'asdf');
+// sum(null, null);
+// sum(undefined, undefined);
+// sum(null, undefined);
+// sum({}, []);
---
+++ add.ts
@@ -0,0 +1,11 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(1, 2));
+// testSum(1, "2");
+// sum('asdf', 'asdf');
+// sum(null, null);
+// sum(undefined, undefined);
+// sum(null, undefined);
+// sum({}, []);
