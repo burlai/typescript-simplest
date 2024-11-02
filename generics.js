fe2c7a2f8d74db5fb155b59547c5d4211ddfeab6
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-11-02 18:07:21 +0100

    ready

--- generics.js
+++ generics.js
@@ -5,3 +5,25 @@ function echo(argument) {
 console.log(echo("Hello, generics!"));
 console.log(echo(36));
 console.log(echo(true));
+const users = [
+    { name: "John", age: 25 },
+    { name: "Jane", age: 19 },
+    { name: "Jack", age: 45 },
+];
+function pluck(array, key) {
+    return array.map((item) => item[key]);
+}
+console.log(pluck(users, "name")); // ["John", "Jane", "Jack"]
+console.log(pluck(users, "age")); // [25, 19, 45]
+// console.log(pluck(users, "email")); // Error - no such key in the object
+// pluck function with meaningful types names
+// function pluck<GenericObjectType, Key extends keyof GenericObjectType>(
+//   array: GenericObjectType[],
+//   keyInTheObject: Key
+// ): GenericObjectType[Key][] {
+//   return array.map((item) => item[keyInTheObject]);
+// }
+// pluck function without generics:
+// function pluckWithoutGenerics(array: any[], key: string): any[] {
+//   return array.map((item) => item[key]);
+// }
--- generics.ts
+++ generics.ts
@@ -1,7 +1,35 @@
-// function echo<T>(argument: T): T {
-//   return argument;
+function echo<T>(argument: T): T {
+  return argument;
+}
+
+console.log(echo("Hello, generics!"));
+console.log(echo(36));
+console.log(echo(true));
+
+const users = [
+  { name: "John", age: 25 },
+  { name: "Jane", age: 19 },
+  { name: "Jack", age: 45 },
+];
+
+function pluck<T, K extends keyof T>(array: T[], key: K): T[K][] {
+  return array.map((item) => item[key]);
+}
+
+console.log(pluck(users, "name")); // ["John", "Jane", "Jack"]
+console.log(pluck(users, "age")); // [25, 19, 45]
+
+// console.log(pluck(users, "email")); // Error - no such key in the object
+
+// pluck function with meaningful types names
+// function pluck<GenericObjectType, Key extends keyof GenericObjectType>(
+//   array: GenericObjectType[],
+//   keyInTheObject: Key
+// ): GenericObjectType[Key][] {
+//   return array.map((item) => item[keyInTheObject]);
 // }
 
-// console.log(echo("Hello, generics!"));
-// console.log(echo(36));
-// console.log(echo(true));
+// pluck function without generics:
+// function pluckWithoutGenerics(array: any[], key: string): any[] {
+//   return array.map((item) => item[key]);
+// }
