7ed253d94d1647e7b8811947b31f0927d6bbaeda
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-11-02 17:48:18 +0100

    echo function

---
+++ generics.js
@@ -0,0 +1,7 @@
+"use strict";
+function echo(argument) {
+    return argument;
+}
+console.log(echo("Hello, generics!"));
+console.log(echo(36));
+console.log(echo(true));
---
+++ generics.ts
@@ -0,0 +1,7 @@
+// function echo<T>(argument: T): T {
+//   return argument;
+// }
+
+// console.log(echo("Hello, generics!"));
+// console.log(echo(36));
+// console.log(echo(true));
