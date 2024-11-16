490843257584b11203fa9ecc2093a7f996427421
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-11-16 14:10:54 +0100

    ready

--- demo.js
+++ demo.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("demo");
+// Union:
+let userId;
+userId = 5;
+userId = "Max";
+// userId = true; // Error
+const greetUser = (user) => {
+    if (typeof user === "string") {
+        console.log(`Hello, ${user.toUpperCase()}!`);
+    }
+    if (typeof user === "number") {
+        console.log(`Hello, user with id ${user}!`);
+    }
+};
+greetUser("Max");
+const superVehicle = {
+    wheels: 4,
+    engine: "V8",
+    sails: 1,
+};
+console.log(superVehicle);
--- demo.ts
+++ demo.ts
@@ -1,1 +1,40 @@
-console.log("demo");
+// Union:
+let userId: string | number;
+
+userId = 5;
+userId = "Max";
+
+// userId = true; // Error
+
+const greetUser = (user: string | number) => {
+  if (typeof user === "string") {
+    console.log(`Hello, ${user.toUpperCase()}!`);
+  }
+  if (typeof user === "number") {
+    console.log(`Hello, user with id ${user}!`);
+  }
+};
+
+greetUser("Max");
+
+// Intersection
+
+type Car = {
+  wheels: number;
+  engine: string;
+};
+
+type Boat = {
+  sails: number;
+  engine: string;
+};
+
+type Amphibia = Car & Boat;
+
+const superVehicle: Amphibia = {
+  wheels: 4,
+  engine: "V8",
+  sails: 1,
+};
+
+console.log(superVehicle);
